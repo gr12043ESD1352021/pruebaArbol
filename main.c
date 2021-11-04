59c0c0911905ca0bb17434d88dd5717c20534493
chaic <chaic@DESKTOP-R9RR6C5>
2021-11-03 21:15:10 -0600

    Prueba arboles 0.5%

--- main.c
+++ main.c
@@ -2,15 +2,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-/*esta es una prueba de git*/
-/*a la verga*/
-/*puita madre*/
 
-/*
- * 
- */
+/*Prueba de git*/
+/*2.0*/
+
 int main(int argc, char** argv) {
-
+    printf("Esta es una prueba de github");
     return (EXIT_SUCCESS);
 }
 
