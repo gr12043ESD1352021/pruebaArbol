70c5c2ffab1e4a8272f135db170dd6ee9cd27f97
chaic <chaic@DESKTOP-R9RR6C5>
2021-11-14 18:47:36 -0600

    Prueba arboles 0.99%

---
+++ Arbol.h
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Arbol.h
+ * Author: chaic
+ *
+ * Created on 3 de noviembre de 2021, 21:16
+ */
+
+#ifndef ARBOL_H
+#define ARBOL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    typedef struct NodoArbol{
+        int dato;
+        struct nodoArbol *izq;
+        struct nodoArbol *der;
+    }nodoArbol;
+    
+    nodoArbol *iniciarArbol();
+    nodoArbol *crearNodoArbol(int dato);
+    nodoArbol *buscar(nodoArbol arbol , int dato);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ARBOL_H */
+
